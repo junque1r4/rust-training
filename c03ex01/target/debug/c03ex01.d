fefc2a9e0becf81df17231852f2e7168f88537e1
Junqueira <dev.junqueira@gmail.com>
2020-10-19 22:40:17 -0300

    Parte 2: estruturas

---
+++ c03ex01/target/debug/c03ex01.d
@@ -0,0 +1,1 @@
+/home/krn/Documents/Codes/Rust/c03ex01/target/debug/c03ex01: /home/krn/Documents/Codes/Rust/c03ex01/src/main.rs
---
+++ c03ex01/target/debug/deps/c03ex01-cbdf1731226dee7c.d
@@ -0,0 +1,5 @@
+/home/krn/Documents/Codes/Rust/c03ex01/target/debug/deps/c03ex01-cbdf1731226dee7c.rmeta: src/main.rs
+
+/home/krn/Documents/Codes/Rust/c03ex01/target/debug/deps/c03ex01-cbdf1731226dee7c.d: src/main.rs
+
+src/main.rs:
