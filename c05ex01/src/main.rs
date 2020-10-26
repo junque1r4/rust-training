53c9e2d9a6b515fc7ec0a27ca240bb6a37bc8f6a
Junqueira <dev.junqueira@gmail.com>
2020-10-26 16:04:02 -0300

    alguns exemplos de sub-rotinas

---
+++ c05ex01/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::io::prelude::*;
+
+fn pausa() {
+    println!();
+    print!("Tecle <ENTER> para encerrar...");
+    io::stdout().flush().unwrap();
+    io::stdin().read_exact(&mut [0u8]).unwrap();
+}
+
+fn fatorial() {
+    let mut n = String::new();
+    let mut f: u64 = 1;
+
+    print!("Entre o valor da fatorial ..: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut n).expect("Erro na entrada");
+    let n: u64 = match n.trim().parse() {
+        Ok(numero) => numero,
+        Err(_) => 0
+    };
+
+    for i in 1 .. n + 1 {
+        f *= i;
+    }
+
+    println!("Resultado ..: {}", f);
+    pausa();
+}
+
+
+fn main() {
+    fatorial();
+}
